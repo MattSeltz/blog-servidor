0456c9af4ddc55bf906db5979153f687fd199fa1
Matt <107654917+MattSeltz@users.noreply.github.com>
2024-07-05 19:00:08 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ export const ENVIRONMENT =
   process.env.ENVIRONMENT ?? `http://localhost:${PORT}`;
 
 export const MONGODB_KEY =
-  process.env.MONGODB_KEY ?? "mongodb://mongo:27017/blog";
+  process.env.MONGODB_KEY ?? "mongodb://localhost:27017/blog";
 
 export const SECRET_KEY = process.env.SECRET_KEY ?? "Matías-Seltzer";
 
