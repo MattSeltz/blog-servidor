b75f81a1ef3c9a3fcd87a39d3587f3df8b7fa2d4
Matt <107654917+MattSeltz@users.noreply.github.com>
2024-07-04 23:38:02 -0300

    Docker implementado

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,22 @@
-import "dotenv/config"
+import "dotenv/config";
 
-export const PORT = process.env.PORT ?? 3000
+export const PORT = process.env.PORT ?? 3000;
 
-export const ENVIRONMENT = process.env.ENVIRONMENT ?? `http://localhost:${PORT}`
+export const ENVIRONMENT =
+  process.env.ENVIRONMENT ?? `http://localhost:${PORT}`;
 
-export const MONGODB_KEY = process.env.MONGODB_KEY ?? "mongodb://localhost:27017/blog"
+export const MONGODB_KEY =
+  process.env.MONGODB_KEY ?? "mongodb://mongo:27017/blog";
 
-export const SECRET_KEY = process.env.SECRET_KEY ?? "Matías-Seltzer"
+export const SECRET_KEY = process.env.SECRET_KEY ?? "Matías-Seltzer";
 
-export const USER_EMAIL = process.env.USER_EMAIL ?? "matiselt@outlook.es"
+export const USER_EMAIL = process.env.USER_EMAIL ?? "matiselt@outlook.es";
 
-export const PASSWORD_EMAIL = process.env.PASSWORD_EMAIL ?? "11144550s"
+export const PASSWORD_EMAIL = process.env.PASSWORD_EMAIL ?? "11144550s";
 
-export const CLOUD_NAME = process.env.CLOUD_NAME ?? "daeqryito"
+export const CLOUD_NAME = process.env.CLOUD_NAME ?? "daeqryito";
 
-export const API_KEY = process.env.API_KEY ?? "793988317468657"
+export const API_KEY = process.env.API_KEY ?? "793988317468657";
 
-export const API_SECRET = process.env.API_SECRET ?? "-zvjpmsxkrMkNKIfObrWo_nZQa4"
+export const API_SECRET =
+  process.env.API_SECRET ?? "-zvjpmsxkrMkNKIfObrWo_nZQa4";
--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.disable("x-powered-by")
 
 app.use(express.json())
 app.use(cors(
-  {origin: "http://localhost:5173", 
+  {origin: "http://localhost", 
   credentials: true}
 ))
 app.use(cookieParser())
